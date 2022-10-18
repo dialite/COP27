0969445caa54a43825e36398e45874c7642ceb71
dialite <raymondolisa01@gmail.com>
2022-10-18 16:00:27 +0100

    Into HTML, css and js files

---
+++ index.js
@@ -0,0 +1,24 @@
+// toggle mobile menu
+const menu = document.getElementById('menu');
+const menuLinks = document.querySelectorAll('#menu a');
+const menuToggler = document.getElementById('menu-toggler');
+const closeMenuBtn = document.getElementById('menu-close-btn');
+
+function toggleMenu(event, prevent = true) {
+  if (prevent) event.preventDefault();
+  document.body.classList.toggle('no-scroll');
+  menu.classList.toggle('show');
+}
+
+menuToggler.addEventListener('click', (event) => {
+  toggleMenu(event);
+});
+
+closeMenuBtn.addEventListener('click', (event) => {
+  toggleMenu(event);
+});
+
+menuLinks.forEach((link) => link.addEventListener(
+  'click',
+  (event) => toggleMenu(event, false),
+));
